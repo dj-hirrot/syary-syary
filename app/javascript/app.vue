657cb54bc9a464d36888f18f97fd01e62217c66b
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-12 17:30:30 +0900

    [update] vueでログイン画面実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -8,6 +8,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import UserNewPage from 'users/New.vue'
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: UserNewPage
+    }
+  ]
+})
+
+Vue.use(VueRouter)
+
 export default {
   router
 }
---
+++ app/javascript/users/New.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent="$emit('submit')">
+    <!-- errors -->
+    <div v-if="errors.length != 0">
+      <ul v-for="e in errors" :key="e">
+        <li><font color="red">{{ e }}</font></li>
+      </ul>
+    </div>
+    <!-- errors -->
+
+    <div>
+      <label>Name</label>
+      <input v-model="user.email" type="text">
+    </div>
+
+    <div>
+      <label>Password</label>
+      <input v-model="user.password" type="password">
+    </div>
+
+    <div>
+      <label>PasswordConfirmation</label>
+      <input v-model="user.password_confirmation" type="password">
+    </div>
+
+    <button type="submit">Commit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+        password_confirmation: ''
+      },
+      errors: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
