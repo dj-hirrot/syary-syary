f834c10ab4d20d5091ab37e1d55ceab4624b3cf9
dj-hirrot <hirotoshibutani@gmail.com>
2019-11-12 17:30:46 +0900

    [update] routeの追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: 'home#index'
+
   namespace :api, { format: :json } do
     namespace :v1 do
       resources :users, only: [:new, :create]
